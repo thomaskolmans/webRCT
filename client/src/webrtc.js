05744d9ebfed131ce30f3e7da6a8251a06e41fbf
Thomas Kolmans <thomas.kolmans@indicia.nl>
2018-12-21 14:35:11 +0100

    Removed vunerablity

--- client/src/webrtc.js
+++ client/src/webrtc.js
@@ -6,6 +6,7 @@ import {BrowserRouter, BrowserHistory, matchPath, Switch, Route, Link} from 'rea
 import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
 import Video from './components/Video';
+import {BUSINESS_NAME, BUSINESS_LOGO_SQUARE} from "babel-dotenv"
 
 
 export default class WebRTC extends React.Component{
@@ -35,8 +36,29 @@ export default class WebRTC extends React.Component{
 }
 const PageShell = Page => { 
     return props =>
-      <div className="page">
-          <Page {...props} />
-      </div>
+		<div className="page">
+			<BasicTags />
+			<Page {...props} />
+		</div>
 };
+
+
+class BasicTags extends React.Component{
+	render(){
+		return(
+			<MetaTags>
+				<title>{BUSINESS_NAME}</title>
+			    <meta name="twitter:title" content={BUSINESS_NAME} />
+			    <meta property="og:title" content={BUSINESS_NAME} />
+			    <meta property="og:site_name" content={BUSINESS_NAME} />
+			    <meta itemProp="name" content={BUSINESS_NAME} />
+
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="shortcut icon" href={BUSINESS_LOGO_SQUARE} />
+			</MetaTags>
+		)
+	}
+}
+
 ReactDOM.render(<WebRTC />,document.getElementById("webrtc"));
